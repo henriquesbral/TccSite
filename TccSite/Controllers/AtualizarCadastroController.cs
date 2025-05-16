1b12f6cac74b277921d9ddc793a09245098d5ddc
Carlos Henrique <henriquesbral@gmail.com>
2025-05-16 17:31:01 -0300

    Subindo o restante das paginas

---
+++ TccSite/Controllers/AtualizarCadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TccSite.Controllers
+{
+    public class AtualizarCadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TccSite/Controllers/PainelAlertasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TccSite.Controllers
+{
+    public class PainelAlertasController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
